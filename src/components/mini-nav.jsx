d338cd93825806baf3a103843d56014f00829148
Emmanueluko3 <emmanueluko90@gmail.com>
2022-11-14 23:25:19 +0100

    key props error fixed

--- src/components/mini-nav.jsx
+++ src/components/mini-nav.jsx
@@ -2,31 +2,31 @@ function Mininav() {
     return ( <>
         <div className="container py-3 ">
             <div className="row text-start">
-                <div className="text-pink py-2 col-lg-1 fs-18 mx-3">
+                <div className="custom-black py-2 col-lg-1 fs-18 mx-3">
                     <a href="#" className="nav-link">Resturant</a>
                 </div>
-                <div className="text-pink py-2 col-lg-1 fs-18 mx-3">
+                <div className="custom-black py-2 col-lg-1 fs-18 mx-3">
                     <a href="#" className="nav-link">Cottage</a>
                 </div>
-                <div className="text-pink py-2 col-lg-1 fs-18 mx-3">
+                <div className="custom-black py-2 col-lg-1 fs-18 mx-3">
                     <a href="#" className="nav-link">Castle</a>
                 </div>
-                <div className="text-pink py-2 col-lg-1 fs-18 mx-3">
+                <div className="custom-black py-2 col-lg-1 fs-18 mx-3">
                     <a href="#" className="nav-link">Fantest city</a>
                 </div>
-                <div className="text-pink py-2 col-lg-1 fs-18 mx-3">
+                <div className="custom-black py-2 col-lg-1 fs-18 mx-3">
                     <a href="#" className="nav-link">Beach</a>
                 </div>
-                <div className="text-pink py-2 col-lg-1 fs-18 mx-3">
+                <div className="custom-black py-2 col-lg-1 fs-18 mx-3">
                     <a href="#" className="nav-link">Cabbins</a>
                 </div>
-                <div className="text-pink py-2 col-lg-1 fs-18 mx-3">
+                <div className="custom-black py-2 col-lg-1 fs-18 mx-3">
                     <a href="#" className="nav-link">Off-grid</a>
                 </div>
-                <div className="text-pink py-2 mb-3 col-lg-1 fs-18 mx-3">
+                <div className="custom-black py-2 mb-3 col-lg-1 fs-18 mx-3">
                     <a href="#" className="nav-link">Farm</a>
                 </div>
-                <div className="text-pink col-lg-1 fs-18 mx-3">
+                <div className="custom-black col-lg-1 fs-18 mx-3">
                     <a href="#" className="nav-link">
                         <div className="card p-1 pointer bg-light width-sm">
                             <div className="d-flex justify-content-between align-items-center px-2">
