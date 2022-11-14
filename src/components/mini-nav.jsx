a20ebfb171577de4028657f570a78f238fab7834
Emmanueluko3 <emmanueluko90@gmail.com>
2022-11-14 01:25:08 +0100

    mini nav in progress

--- src/components/mini-nav.jsx
+++ src/components/mini-nav.jsx
@@ -2,15 +2,23 @@ function Mininav() {
     return ( <>
         <div className="container py-3">
             <div className="row">
-                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-3">Resturant</a>
-                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-3">Cottage</a>
-                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-3">Castle</a>
-                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-3">Fantest city</a>
-                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-3">Beach</a>
-                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-3">Cabbins</a>
-                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-3">Off-grid</a>
-                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-3">Farm</a>
-                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-3">Resturant</a>
+                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-2">Resturant</a>
+                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-2">Cottage</a>
+                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-2">Castle</a>
+                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-2">Fantest city</a>
+                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-2">Beach</a>
+                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-2">Cabbins</a>
+                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-2">Off-grid</a>
+                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-2">Farm</a>
+                <a href="#" className="nav-link text-pink py-2 col-lg-1 mx-2">Resturant</a>
+                <div className="card p-1 pointer bg-light width-sm">
+                    {/* <div className="d-flex justify-content-between align-items-center px-2">
+                        
+                            <h4 className="fw-normal">Location</h4>
+                       
+                        <i className="fa-solid fa-chevron-right"></i>
+                    </div> */}
+                </div>
             </div>
         </div>
     </> );
--- src/pages/place-to-stay.jsx
+++ src/pages/place-to-stay.jsx
@@ -1,8 +1,182 @@
 import Mininav from "../components/mini-nav";
+import Dommycard from '../components/cards';
+import Card1 from '../img/card5.svg';
+import Card2 from '../img/card6.svg';
+import Card3 from '../img/card7.svg';
+import Card4 from '../img/card8.svg';
+import Card5 from '../img/card1.svg';
+import Card6 from '../img/card2.svg';
+import Card7 from '../img/card3.svg';
+import Card8 from '../img/card4.svg';
+import Card9 from '../img/card9.svg';
+import Card10 from '../img/card10.svg';
+import Card11 from '../img/card11.svg';
+import Card12 from '../img/card12.svg';
+import Card13 from '../img/card13.svg';
+import Card14 from '../img/card14.svg';
+import Card15 from '../img/card15.svg';
+import Card16 from '../img/card16.svg';
 
 function PlaceToStay() {
+
+    const cardItems = [
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card1
+        }, 
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card2 
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card3
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card4
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card5
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card6
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card7
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card8
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card9
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card10
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card11
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card12
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card13
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card14
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card15
+        },
+        {
+            title: 'Desert King',
+            per_night: '1MBT per night',
+            distance: '2345km away',
+            availability: 'available for 2weeks stay',
+            star_number: 5,
+            image:Card16
+        },
+    ]
+
+    const cards = cardItems.map((item, count) => (
+        <div className="col-lg-3">
+            <div className="d-flex justify-content-center">
+                <Dommycard 
+                key={item.toString() + '-' + count}
+                title={item.title} 
+                per_night={item.per_night}
+                distance={item.distance}
+                availability={item.availability}
+                star_number={item.star_number}
+                image={item.image} 
+                />
+            </div>
+            
+        </div>
+    ))
+
     return ( <>
-            <Mininav />
+            <div className="container text-center py-5">
+                <Mininav />
+                <div className="py-3">
+                    <div className="row">
+                        <>{cards}</>
+                    </div>
+                    
+                </div>
+            </div>
     </> );
 }
 
